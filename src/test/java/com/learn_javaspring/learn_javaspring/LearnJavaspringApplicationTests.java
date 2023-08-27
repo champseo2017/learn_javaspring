1e6c8dcd5207e1a61b99733abcf7bf7ea4ee4c59
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2023-08-27 15:19:13 +0700

    at Chapter 2: Caching with Redis Using Spring Boot for MongoDB

--- src/test/java/com/learn_javaspring/learn_javaspring/LearnJavaspringApplicationTests.java
+++ src/test/java/com/learn_javaspring/learn_javaspring/LearnJavaspringApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.learn_javaspring.learn_javaspring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class LearnJavaspringApplicationTests {
 
 	@Test
 	void contextLoads() {
